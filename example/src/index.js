e0927348e997a94974e3315e068a044d5ade33b0
Juliano Rodrigues <juliano.silva.rodrigues@gmail.com>
2019-01-14 23:27:26 +0000

    Final Version

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Appear, Deck, Heading, Image, Slide } from '../../src';
+import { Appear, Deck, Heading, Image, Slide, MarkdownSlides, List, ListItem } from '../../src';
 import preloader from '../../src/utils/preloader';
 import createTheme from '../../src/themes/default';
 
@@ -8,25 +8,23 @@ require('normalize.css');
 
 const images = {
   city: require('../assets/city.jpg'),
-  facebook: require('../assets/Facebook_2.png'),
-  facebook2: require('../assets/Facebook_3.png'),
   htmljscss: require('../assets/hcj.png'),
   darth: require('../assets/darth_vader.jpg'),
   jedi: require('../assets/master.gif'),
   thank: require('../assets/thanks.gif'),
   html: require('../assets/html.png'),
-  htmlexample: require('../assets/html_example.png'),
-  cssexample: require('../assets/css_example.png'),
   jsexample: require('../assets/js_example.png'),
   css: require('../assets/css.png'),
   js: require('../assets/js.png'),
-  luke: require('../assets/Luke.gif')
+  luke: require('../assets/Luke.gif'),
+  twitter: require('../assets/twitter.png'),
+  twitter2: require('../assets/twitter2.png')
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: '#ccc'
+  primary: '#DCDCDC'
 });
 
 export default class Presentation extends Component {
@@ -54,7 +52,7 @@ export default class Presentation extends Component {
         theme={theme}
       >
         <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} caps lineHeight={1} textColor="white">
+          <Heading size={1} caps lineHeight={1} textColor="black">
             FRONTEND WARS
           </Heading>
           <Heading size={5} textColor="red">
@@ -62,35 +60,36 @@ export default class Presentation extends Component {
           </Heading>
         </Slide>
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="white" caps>
+          <Heading size={6} textColor="black" caps>
             you don't know the power of the frontend side
           </Heading>
           <Appear>
             <Image
-              src={images.facebook.replace('/', '')}
-              trasform="scale(0.5, 0.5)"
+              src={images.twitter.replace('/', '')}
+              height="700px"
+              width="700px"
             />
           </Appear>
         </Slide>
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
+          <Heading size={6} textColor="black" caps>
             The Force is Strong With This One
           </Heading>
           <Appear>
             <Image
-              src={images.facebook2.replace('/', '')}
-              margin="0px auto 40px"
+              src={images.twitter2.replace('/', '')}
+              height="700px"
+              width="700px"
             />
           </Appear>
         </Slide>
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
+          <Heading size={6} textColor="black" caps>
             The Force
           </Heading>
           <Appear>
             <Image
               src={images.htmljscss.replace('/', '')}
-              margin="0px auto 40px"
             />
           </Appear>
         </Slide>
@@ -99,42 +98,119 @@ export default class Presentation extends Component {
             <Image src={images.html.replace('/', '')} margin="0px auto 40px" />
           </Appear>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Image src={images.htmlexample.replace('/', '')} />
-        </Slide>
+        {MarkdownSlides`
+Example
+\`\`\`html
+<div ng-controller="AccountMenuCtrl">
+    <div class="row main-menu-title" ng-init="accountMenu = 'user';accountSubmenu = 'editProfile'">
+        <h1 role="heading" aria-level="1">{{'account.edit.title.editYourProfile' | i18n}}</h1>
+    </div>
+    <div class="row main-view">
+    <!-- Loading templates depending of the viewport size -->
+        <div class="columns medium-12 small-12 main-menu-primary hide-for-large-up">
+            <div ng-include="'account/menu.tpl.html'" ng-repeat="contentTemplate in ['account/editprofile-content.tpl.html']"></div>
+        </div>
+        <div class="show-for-large-up">
+            <div class="columns large-3 main-menu-primary">
+                <div ng-include="'account/menu.tpl.html'"></div>
+            </div>
+            <div class="categoryNavigation columns large-9 main-menu-grid account-content">
+                <div class="bk-padding-top" ng-if="!small" ng-include="'account/editprofile-content.tpl.html'"></div>
+            </div>
+        </div>
+    </div>
+</div>
+\`\`\`
+        `}
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
           <Appear>
             <Image src={images.css.replace('/', '')} margin="0px auto 40px" />
           </Appear>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Appear>
-            <Image
-              src={images.cssexample.replace('/', '')}
-              margin="0px auto 40px"
-            />
-          </Appear>
-        </Slide>
+{MarkdownSlides`
+Example
+\`\`\`css
+    text-decoration: none;
+    position: relative;
+    text-transform: uppercase;
+    text-shadow: none;
+    &::before {
+        color: black;
+        content: attr(data-hover);
+        position: absolute;
+        -webkit-transition: -webkit-transform 0.3s, opacity 0.3s;
+        -moz-transition: -moz-transform 0.3s, opacity 0.3s;
+        transition: transform 0.3s, opacity 0.3s;
+    }
+    &:hover::before,
+    &:focus::before {
+        color: white;
+        -webkit-transform: scale(0.9);
+        -moz-transform: scale(0.9);
+        transform: scale(0.9);
+    }
+\`\`\`
+`}
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
           <Appear>
             <Image src={images.luke.replace('/', '')} margin="0px auto 40px" />
           </Appear>
+        </Slide>
+        <Slide>
+          <Heading size={4} textColor="black" caps>
+            CSS preprocessors
+          </Heading>
+          <List>
+            <Appear>
+              <ListItem>SASS</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>LESS</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>POSTCSS</ListItem>
+            </Appear>
+        </List>
         </Slide>
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
           <Appear>
             <Image src={images.js.replace('/', '')} margin="0px auto 40px" />
           </Appear>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+        {MarkdownSlides`
+Example
+\`\`\`js
+storeService.isStoreOpen({'storeName': restaurant.storeName}).then(function (result) {
+  storeStatus = result.storeStatus;
+  if (storeStatus !== 'ACTIVE') {
+      historyService.goTo($rootScope.appConfig.defaultScreenPath);
+  } else {
+      cartResource.calculateTotals();
+      deferred.resolve();
+  }
+  }).catch(function (error) {
+      deferred.reject(emn8.Util.DevLogger(error));
+  });
+\`\`\`
+        `}
+        <Slide>
+          <Heading size={4} textColor="black" caps>
+            Frameworks
+          </Heading>
+          <List>
           <Appear>
-            <Image
-              src={images.jsexample.replace('/', '')}
-              margin="0px auto 40px"
-            />
+            <ListItem>React.js</ListItem>
           </Appear>
+          <Appear>
+            <ListItem>Angular</ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>Vue.js</ListItem>
+          </Appear>
+        </List>
         </Slide>
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
+          <Heading size={6} textColor="black" caps>
             Join the FRONTEND Side
           </Heading>
           <Appear>
@@ -142,7 +218,7 @@ export default class Presentation extends Component {
           </Appear>
         </Slide>
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
+          <Heading size={6} textColor="black" caps>
             and became a Frontend master
           </Heading>
           <Appear>
